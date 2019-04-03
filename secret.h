27ba0049f3d5bfcb0407384969ba7a96b9382f93
Yu Lian Ng <yulian.ng@thoughtworks.com>
2019-04-04 00:31:59 +0800

    ignore updates to secret.h, unstage path from index

--- secret.h
+++
@@ -1,6 +0,0 @@
-const char* ssid     = "ssid";
-const char* password = "password";
-
-const char* mqttClientId  = "mqttClient";
-const char* mqttUsername = "mqttUser";
-const char* mqttPassword = "mqttPassword";
